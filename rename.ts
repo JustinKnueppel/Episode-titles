9bf9155f6536f00948488183b4c8552a9b1f97e1
Justin Knueppel <justinknueppel@gmail.com>
2020-09-16 09:09:03 -0400

    fix rename script

--- rename.ts
+++ rename.ts
@@ -1,4 +1,7 @@
 import { promises as fs } from "fs";
+import path from "path";
+
+const ROOT_DIR="/home/justink/Public/Videos/TV/Modern Family"
 
 interface Episode {
   episode: Number;
@@ -18,11 +21,11 @@ const getShowData = async (filename: string): Promise<Array<Season>> => {
 const getEpisodeAndExtension = (
   filename: string
 ): { episode: string; extension: string } => {
-  const match = filename.match(/^.*S\d+E(\d+).*\.(.+)$/);
+  const match = filename.match(/^.*[sS]\d+[eE](\d+).*\.(.+)$/);
   if (match) {
     return {
-      episode: match[0],
-      extension: match[1],
+      episode: match[1],
+      extension: match[2],
     };
   }
   return {
@@ -33,19 +36,20 @@ const getEpisodeAndExtension = (
 
 const main = async () => {
   const showData = await getShowData("modern_family.json");
+  process.chdir(ROOT_DIR);
   showData.forEach(async (season) => {
-    process.chdir(`Season ${season}`);
-    const currentTitles = await fs.readdir(".");
+    const seasonString = season.season < 10 ? `0${season.season}` : season.season.toString();
+    const seasonDirectory = `Season ${seasonString}`;
+    const currentTitles = await fs.readdir(seasonDirectory);
     currentTitles.forEach(async (currentTitle) => {
       const { episode, extension } = getEpisodeAndExtension(currentTitle);
       const episodeTitle = season.episodes.find(
         (e) => e.episode === Number(episode)
       )?.title;
       const newTitle = `Episode ${episode} - ${episodeTitle}.${extension}`;
-      console.log({ currentTitle, newTitle });
+      console.log({ currentTitle: path.join(seasonDirectory, currentTitle), newTitle: path.join(seasonDirectory, newTitle) });
       // await fs.rename(currentTitle, newTItle)
     });
-    process.chdir("..");
   });
 };
 
